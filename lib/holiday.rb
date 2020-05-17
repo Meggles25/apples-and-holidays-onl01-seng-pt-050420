4e8adce824060d354156893cc7c3392b1ee70744
Megan Gomes <megan.gomes25@gmail.com>
2020-05-17 18:30:02 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -32,9 +32,10 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
      if season == :winter
        holiday_names_hash.each do |holiday_name, supplies|
          supplies << supply
-
+       end
+     end  
+  end
 end
-
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
