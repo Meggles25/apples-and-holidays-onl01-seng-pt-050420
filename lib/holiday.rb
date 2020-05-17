896622c088a2f6ffe280776d6f9aa3bc2a3fe952
Megan Gomes <megan.gomes25@gmail.com>
2020-05-17 20:31:34 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -93,7 +93,7 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
 
   bbq_array = []
-  holiday_hash.each do |holiday, supplies|
+  holiday_hash.each do |season, holiday_supplies_hash|
     holiday_supplies_hash.each do |holiday, supplies|
       if supplies.include?("BBQ")
         bbq_array << holiday
