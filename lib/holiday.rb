0ba20ce3212d96bb2040edccd774c4fc99b79fa0
Megan Gomes <megan.gomes25@gmail.com>
2020-05-17 18:38:58 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -23,6 +23,7 @@ def second_supply_for_fourth_of_july(holiday_hash)
 
 end
 
+
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
@@ -37,11 +38,13 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   end
 end
 
+
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
 
 end
+
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
@@ -49,10 +52,12 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
 
 end
 
+
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
 
 end
+
 
 def all_supplies_in_holidays(holiday_hash)
   # iterate through holiday_hash and print items such that your readout resembles:
@@ -64,6 +69,7 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
 
 end
+
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
